552634d7b10d327aa3b22bfebf15fee3910a24af
Notfamousyet <shreyapawale87@gmail.com>
2026-08-01 18:24:00 +0530

    .

--- Basic/JavaScript/part1/loops.js
+++ Basic/JavaScript/part1/loops.js
@@ -56,3 +56,116 @@ for(let i=0;i<citiees.length;i++){
     citylist[i]=citiees[i];
 }
 console.log(citylist);
+
+let teastall=["green tea","black tea","chai","herbal tea","oolong tea"];
+let selectedtea=[]
+for(let i=0;i<teastall.length;i++){
+    if(teastall[i]!="chai"){
+        selectedtea[i]=teastall[i];
+    }
+    else{
+        break;
+    }
+}
+console.log(selectedtea);
+
+let countylist=["london","new york","paris","berlin"];
+let visitedcities=[];
+for(let i=0;i<countylist.length;i++){
+    if(countylist[i]=="paris"){
+        continue;
+    }
+    visitedcities.push(countylist[i]);
+}
+console.log(visitedcities);
+
+let numbers=[1,2,3,4,5]
+let smallnumbers=[]
+for (const num of numbers) {
+    if(num===4){
+        break;
+    }
+    smallnumbers.push(num);
+}
+console.log(smallnumbers);
+
+let newteastall=[]
+for (const teas of teastall) {
+    if(teas=="herbal tea"){
+        continue;
+    }
+    newteastall.push(teas);
+}
+console.log(newteastall)
+
+let citypopulation={
+    "london":100000000,
+    "new york":20000000,
+    "paris":30000000,
+    "berlin":4000000
+};
+let citypopulations={}
+for (const city in citypopulation) {
+    console.log(city);
+}
+for (const city in citypopulation) {
+    if(city==="berlin"){
+        continue;
+    }
+    citypopulations[city]=citypopulation[city];
+}
+console.log(citypopulations)
+
+let teacollection=["earl grey","green tea","chai","oolong tea"];
+let availableteas=[];
+teacollection.forEach(function(tea){
+    if(tea==='chai'){
+        return;
+    }
+    availableteas.push(tea);
+})
+console.log(availableteas);
+
+let tovisit=["berlin","tokyo","sydney","paris"];
+let traveledcities=[]
+tovisit.forEach(function(city) {
+    if(city==="sydney"){
+        return;
+    }
+    traveledcities.push(city);
+});
+console.log(traveledcities);
+
+let teasection=["chai","green tea","black tea","jasmin tea","herbal tea","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"];
+let shorttea=[];
+for (const teas of teasection) {
+    if(teas.length<=10){
+        shorttea.push(teas);
+    }
+}
+console.log(shorttea);
+
+function makeTea(typeOfTea){
+    return `Making ${typeOfTea}`;
+}
+let teaorder=makeTea("lemon tea");
+console.log(teaorder);
+
+function ordertea(teatype){
+    function confirmorder(){
+        return `order confirmed for ${teatype}`
+    }
+    return confirmorder();
+}
+let orderconfirmation =ordertea("chai");
+console.log(orderconfirmation);
+
+const calculateTotal=(price,quality)=>{
+    return price * quality
+}
+let totalcost=calculateTotal(499,100);
+console.log(totalcost);
+
+function processteaorder(){
+    
+}
